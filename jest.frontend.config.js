8e85a68f71ef7878e5304c2cd1940f89820a624d
Victoria281 <45333367+Victoria281@users.noreply.github.com>
2026-02-09 03:56:00 +0800

    Move Product pages into Product folder and Configure Test Environment

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -20,11 +20,12 @@ export default {
   transformIgnorePatterns: ["/node_modules/(?!(styleMock\\.js)$)"],
 
   // only run these tests
-  testMatch: ["<rootDir>/client/src/pages/Auth/*.test.js"],
+  testMatch: ["<rootDir>/client/src/pages/Auth/*.test.js",
+    "<rootDir>/client/src/pages/Product/*.test.js"],
 
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["client/src/pages/Auth/**"],
+  collectCoverageFrom: ["client/src/pages/Auth/**", "client/src/pages/Product/**"],
   coverageThreshold: {
     global: {
       lines: 100,
