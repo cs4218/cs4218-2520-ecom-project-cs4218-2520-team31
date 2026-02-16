efe46fbf650db7a31f9b7ecfb88983f5d0513e97
brennalaurentan <122245137+brennalaurentan@users.noreply.github.com>
2026-02-16 22:41:18 +0800

    Fix orders.map React list warning

--- client/src/pages/user/Orders.js
+++ client/src/pages/user/Orders.js
@@ -31,14 +31,14 @@ const Orders = () => {
             <h1 className="text-center">All Orders</h1>
             {orders?.map((o, i) => {
               return (
-                <div className="border shadow">
+                <div className="border shadow" key={o._id}>
                   <table className="table">
                     <thead>
                       <tr>
                         <th scope="col">#</th>
                         <th scope="col">Status</th>
                         <th scope="col">Buyer</th>
-                        <th scope="col"> date</th>
+                        <th scope="col">Date</th>
                         <th scope="col">Payment</th>
                         <th scope="col">Quantity</th>
                       </tr>
