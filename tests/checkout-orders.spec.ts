7de7b35b4dce73e32fd434d09dac9670c62cd291
brennalaurentan <122245137+brennalaurentan@users.noreply.github.com>
2026-03-22 20:43:55 +0800

    Refactor integration tests for user purchase flow

--- tests/checkout-orders.spec.ts
+++ tests/checkout-orders.spec.ts
@@ -3,8 +3,8 @@
 import { test, expect } from '@playwright/test';
 
 const TEST_USER = {
-  email: "e0959380@u.nus.edu",
-  password: "passpass",
+  email: "test@mail.com",
+  password: "testtest",
 }
 
 const TEST_CREDIT_CARD = {
@@ -13,48 +13,59 @@ const TEST_CREDIT_CARD = {
   cvv: "123",
 }
 
-test('user can complete purchase and see order, ', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+test.describe("User Purchase Flow", () => {
+  test.setTimeout(30000);
 
-  // login
-  await page.getByRole('link', { name: 'Login' }).click();
-  await page.getByRole('textbox', { name: 'Enter Your Email' }).click();
-  await page.getByRole('textbox', { name: 'Enter Your Email' }).fill(TEST_USER.email);
-  await page.getByRole('textbox', { name: 'Enter Your Password' }).click();
-  await page.getByRole('textbox', { name: 'Enter Your Password' }).fill(TEST_USER.password);
-  await page.getByRole('button', { name: 'LOGIN' }).click();
-
-  // add item to cart
-  await page.getByTestId('add-to-cart-the-law-of-contract-in-singapore').click();
-
-  // go to cart
-  await page.getByRole('link', { name: 'Cart' }).click();
-
-  // pay
-  await page.getByRole('button', { name: 'Paying with Card' }).click();
-
-  // enter credit card details
-  await page.locator('iframe[name="braintree-hosted-field-number"]').contentFrame().getByRole('textbox', { name: 'Credit Card Number' }).click();
-  await page.locator('iframe[name="braintree-hosted-field-number"]').contentFrame().getByRole('textbox', { name: 'Credit Card Number' }).fill(TEST_CREDIT_CARD.card_number);
-  await page.locator('iframe[name="braintree-hosted-field-expirationDate"]').contentFrame().getByRole('textbox', { name: 'Expiration Date' }).click();
-  await page.locator('iframe[name="braintree-hosted-field-expirationDate"]').contentFrame().getByRole('textbox', { name: 'Expiration Date' }).fill(TEST_CREDIT_CARD.expiry_date);
-  await page.locator('iframe[name="braintree-hosted-field-cvv"]').contentFrame().getByRole('textbox', { name: 'CVV' }).click();
-  await page.locator('iframe[name="braintree-hosted-field-cvv"]').contentFrame().getByRole('textbox', { name: 'CVV' }).fill(TEST_CREDIT_CARD.cvv);
-
-  // submit payment
-  await page.getByRole('button', { name: 'Make Payment' }).click();
-
-  // navigate to orders page
-  await page.goto('http://localhost:3000/dashboard/user/orders');
-
-  // check for correct orders page
-  await expect(page.getByRole('heading', { name: /all orders|orders/i })).toBeVisible();
-
-  // check for successful order block
-  const successOrderBlock = page.locator('div.border.shadow').filter({
-    hasText: 'Success',
+  test.beforeEach(async ({ page }) => {
+    // navigate to landing page
+    await page.goto('http://localhost:3000/');
   });
 
-  // check for ordered item
-  await expect(successOrderBlock.getByText('The Law of Contract in').last()).toBeVisible();
+  test('user can complete purchase and see order', async ({ page }) => {
+
+    // login
+    await page.getByRole('link', { name: 'Login' }).click();
+    await page.getByRole('textbox', { name: 'Enter Your Email' }).click();
+    await page.getByRole('textbox', { name: 'Enter Your Email' }).fill(TEST_USER.email);
+    await page.getByRole('textbox', { name: 'Enter Your Password' }).click();
+    await page.getByRole('textbox', { name: 'Enter Your Password' }).fill(TEST_USER.password);
+    await page.getByRole('button', { name: 'LOGIN' }).click();
+
+    // add item to cart
+    await page.getByTestId('add-to-cart-the-law-of-contract-in-singapore').click();
+
+    // go to cart
+    await page.getByRole('link', { name: 'Cart' }).click();
+
+    // pay
+    await page.getByRole('button', { name: 'Paying with Card' }).click();
+
+    // enter credit card details
+    await page.locator('iframe[name="braintree-hosted-field-number"]').contentFrame().getByRole('textbox', { name: 'Credit Card Number' }).click();
+    await page.locator('iframe[name="braintree-hosted-field-number"]').contentFrame().getByRole('textbox', { name: 'Credit Card Number' }).fill(TEST_CREDIT_CARD.card_number);
+    await page.locator('iframe[name="braintree-hosted-field-expirationDate"]').contentFrame().getByRole('textbox', { name: 'Expiration Date' }).click();
+    await page.locator('iframe[name="braintree-hosted-field-expirationDate"]').contentFrame().getByRole('textbox', { name: 'Expiration Date' }).fill(TEST_CREDIT_CARD.expiry_date);
+    await page.locator('iframe[name="braintree-hosted-field-cvv"]').contentFrame().getByRole('textbox', { name: 'CVV' }).click();
+    await page.locator('iframe[name="braintree-hosted-field-cvv"]').contentFrame().getByRole('textbox', { name: 'CVV' }).fill(TEST_CREDIT_CARD.cvv);
+
+    // submit payment
+    await page.getByRole('button', { name: 'Make Payment' }).click();
+
+    // navigate to orders page
+    await page.goto('http://localhost:3000/dashboard/user/orders');
+
+    // click orders button
+    await page.getByText('Orders').nth(1).click();
+
+    // check for correct orders page
+    await expect(page.getByRole('heading', { name: /all orders|orders/i })).toBeVisible();
+
+    // check for successful order block
+    const successOrderBlock = page.locator('div.border.shadow').filter({
+      hasText: 'Success',
+    });
+
+    // check for ordered item
+    await expect(successOrderBlock.getByText('The Law of Contract in').last()).toBeVisible();
+  });
 });
